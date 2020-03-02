b716ca5ddf22456b000f18a0bf27700a1d611846
riddhasoft1008 <sudeep.uprety@apexcollege.edu.np>
2020-03-02 08:20:06 +0545

    added services for customer

---
+++ ApexRestaurant.Services/SCustomer/ICustomerServices.cs
@@ -0,0 +1,11 @@
+using ApexRestaurant.Services.SCustomer;
+using ApexRestaurant.Repository.Domain;
+using ApexRestaurant.Repository.RCustomer; 
+ 
+namespace ApexRestaurant.Services.SCustomer 
+{ 
+    public interface ICustomerServices : IGenericService<Customer> 
+    { 
+ 
+    } 
+} 
